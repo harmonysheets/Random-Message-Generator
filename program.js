b99121df17e713a9b72942257e84a6b37696a8af
Moshe Rosenberg <82032539+mosherosenberg@users.noreply.github.com>
2021-04-08 14:24:47 +0300

    Program made much simplier and complete

--- program.js
+++ program.js
@@ -4,83 +4,12 @@ function getRandomMessage(){
     const numTwo = Math.floor(Math.random() * 5);
     const numThree = Math.floor(Math.random() * 5);
 
-    const msgPartOne = ['Think about ', 'You need to know ', "I'm sure ", 'You know ', 'I bet '];
-    const msgPartTwo = ['Der heilige Bashefer ', 'God ', 'Hashem ', 'Totty ', 'Hakudosh Boruch Hu '];
-    const msgPartThree = ['Loves when you talk to him!', 'Loves you!', 'Is with you!', 'Is everywhere!', 'is the best!'];
+    const msgPartOne = ['Think about', 'You need to know', "I'm sure", 'You know', 'I bet'];
+    const msgPartTwo = ['the almighty God', 'God', 'the father in heaven', 'the creator of the world', 'our great God'];
+    const msgPartThree = ['loves when you talk to him!', 'loves you!', 'is with you!', 'is everywhere!', 'is the best!'];
 
-    var wholeMessage;
+    console.log(`${msgPartOne[numOne]} ${msgPartTwo[numTwo]} ${msgPartThree[numThree]}`)
 
-    //adds the first part of the message to wholeMessage var.
-    switch (numOne) {
-        case 0:
-            wholeMessage = msgPartOne[0];
-            break;
-            case 1:
-            wholeMessage = msgPartOne[1];
-            break;
-            case 2:
-            wholeMessage = msgPartOne[2];
-            break;
-            case 3:
-            wholeMessage = msgPartOne[3];
-            break;
-            case 4:
-            wholeMessage = msgPartOne[4];
-            break;
-    
-        default:
-            wholeMessage = 'The was a problem with part 1 of the message. '
-            break;
-    }
-
-    //adds the second part of the message to wholeMessage var.
-    switch (numTwo) {
-        case 0:
-                wholeMessage = wholeMessage + msgPartTwo[0];
-            break;
-            case 1:
-                wholeMessage = wholeMessage + msgPartTwo[1];
-            break;
-            case 2:
-                wholeMessage = wholeMessage + msgPartTwo[2];
-            break;
-            case 3:
-                wholeMessage = wholeMessage + msgPartTwo[3];
-            break;
-            case 4:
-                wholeMessage = wholeMessage + msgPartTwo[4];
-            break;
-    
-        default:
-            wholeMessage = wholeMessage + 'The was a problem with part 2 of the message. '
-            break;
-    }
-
-    //adds the third part of the message to wholeMessage var.
-    switch (numThree) {
-        case 0:
-                wholeMessage = wholeMessage + msgPartThree[0];
-            break;
-            case 1:
-                wholeMessage = wholeMessage + msgPartThree[1];
-            break;
-            case 2:
-                wholeMessage = wholeMessage + msgPartThree[2];
-            break;
-            case 3:
-                wholeMessage = wholeMessage + msgPartThree[3];
-            break;
-            case 4:
-                wholeMessage = wholeMessage + msgPartThree[4];
-            break;
-    
-        default:
-            wholeMessage = wholeMessage + 'The was a problem with part 3 of the message. '
-            break;
-    }
-    
-    console.log(wholeMessage);
-    return wholeMessage;
 }
 
 getRandomMessage();
