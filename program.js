81fbebb3bdf7bd99402c35540c386268a99cbe50
Moshe Rosenberg <82032539+mosherosenberg@users.noreply.github.com>
2021-04-08 14:04:00 +0300

    Project is complete, program shows a random 3 step message every time it runs.

--- program.js
+++ program.js
@@ -10,6 +10,7 @@ function getRandomMessage(){
 
     var wholeMessage;
 
+    //adds the first part of the message to wholeMessage var.
     switch (numOne) {
         case 0:
             wholeMessage = msgPartOne[0];
@@ -31,7 +32,55 @@ function getRandomMessage(){
             wholeMessage = 'The was a problem with part 1 of the message. '
             break;
     }
+
+    //adds the second part of the message to wholeMessage var.
+    switch (numTwo) {
+        case 0:
+                wholeMessage = wholeMessage + msgPartTwo[0];
+            break;
+            case 1:
+                wholeMessage = wholeMessage + msgPartTwo[1];
+            break;
+            case 2:
+                wholeMessage = wholeMessage + msgPartTwo[2];
+            break;
+            case 3:
+                wholeMessage = wholeMessage + msgPartTwo[3];
+            break;
+            case 4:
+                wholeMessage = wholeMessage + msgPartTwo[4];
+            break;
+    
+        default:
+            wholeMessage = wholeMessage + 'The was a problem with part 2 of the message. '
+            break;
+    }
+
+    //adds the third part of the message to wholeMessage var.
+    switch (numThree) {
+        case 0:
+                wholeMessage = wholeMessage + msgPartThree[0];
+            break;
+            case 1:
+                wholeMessage = wholeMessage + msgPartThree[1];
+            break;
+            case 2:
+                wholeMessage = wholeMessage + msgPartThree[2];
+            break;
+            case 3:
+                wholeMessage = wholeMessage + msgPartThree[3];
+            break;
+            case 4:
+                wholeMessage = wholeMessage + msgPartThree[4];
+            break;
+    
+        default:
+            wholeMessage = wholeMessage + 'The was a problem with part 3 of the message. '
+            break;
+    }
+    
     console.log(wholeMessage);
+    return wholeMessage;
 }
-///fdgdg
-console.log('Hello Programmer!')
+
+getRandomMessage();
